c9ebc23a7229054dd0e55bb16ad7a3f83097c72f
Flavio Roth <flavio@secondspectrum.com>
2022-10-06 13:45:42 +0200

    Adds av_seek_frame support

--- src/format/mod.rs
+++ src/format/mod.rs
@@ -12,7 +12,7 @@ pub use self::context::Context;
 pub mod format;
 #[cfg(not(feature = "ffmpeg_5_0"))]
 pub use self::format::list;
-pub use self::format::{flag, Flags};
+pub use self::format::{flag, SeekFlags, Flags};
 pub use self::format::{Input, Output};
 
 pub mod network;
